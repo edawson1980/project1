0da9273c764e4d9faac3ea5ebe6e7c987f9db3bb
Erin <erinedawson@gmail.com>
2017-04-03 12:49:29 -0400

    add toggle feature for colors

---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(".blue").click(function () {
+   $(this).toggleClass("dkblue");
+});
+
+$(".yellow").click(function () {
+   $(this).toggleClass("dkyellow");
+});
+
+$(".red").click(function () {
+   $(this).toggleClass("dkred");
+});
+
+$(".green").click(function () {
+   $(this).toggleClass("dkgreen");
+});
+
+console.log("hello");
