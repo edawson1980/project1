07e04ee95b216a8de93252881c5b2e5b839dc767
Erin <erinedawson@gmail.com>
2017-04-05 13:54:43 -0400

    set timeout for div.blue

--- script3.js
+++ script3.js
@@ -17,52 +17,88 @@ var compMoves = 0;
 var userMoves = 0;
 var score = 0;
 
-var active = function (){
-  (index.pattern).toggleClass('active');
-};
-
-// lights.forEach(function(currentLight){
-//   console.log("this is called");
-//   currentLight.toggleClass('active');
-// });
-
 
 var randomColor = function(){
   console.log("colors work");
   return lights[Math.floor(Math.random()*lights.length)];
 };
- compMoves = function(){
-   pattern.push(randomColor());
- };
 
+ $(".midbutton").click(function(){
+   console.log("button works");
+   $(blue).addClass("active");
+   setTimeout(function(){
+  $(blue).removeClass("active");
+   }, 250);
+  });
 
-   blue.addClass("active");
-   //timer//
-   blue.removeClass("active");
-
-   yellow.addClass("active");
-   //timer//
-   yellow.removeClass("active");
-
-   red.addClass("active");
-   //timer//
-   red.removeClass("active");
-
-   green.addClass("active");
-   //timer//
-   green.removeClass("active");
+  compMoves = function(){
+    pattern.push(randomColor());
 
 
 
-var isClicked = setInterval(active, 750);
-  clearInterval(isClicked);
+   for (i = 0; i < pattern.length; i++){
+     if (pattern[i] == blue){
+       blue.addClass("active");
+       //timer//
+       blue.removeClass("active");
+    } else if (pattern[i] == yellow){
+      yellow.addClass("active");
+      //timer//
+      yellow.removeClass("active");
+    } else if (pattern[i] == red){
+      red.addClass("active");
+      //timer//
+      red.removeClass("active");
+    } else if (pattern[i] == green){
+      green.addClass("active");
+      //timer//
+      green.removeClass("active");
+    } else {
 
-$(".midbutton").click(function(){
-  console.log("button works");
-});
+    }
+  }
+  };
+
+
+  userMoves = function(){
+    $(".lights").click(function(){
+      console.log("button works");
+      });
+  };
+
+
+
+// var isClicked = setInterval(active, 750);
+//   clearInterval(isClicked);
+
+var userBlue = function(){
+  $(blue).click(function(){
+    $(".blue").toggleClass("active");
+  });
+};
+
+var userYellow = function(){
+  $(yellow).click(function(){
+    $(".yellow").toggleClass("active");
+  });
+};
+
+var userRed = function(){
+  $(red).click(function(){
+    $(".red").toggleClass("active");
+  });
+};
+
+var userGreen = function(){
+  $(green).click(function(){
+    $(".green").toggleClass("active");
+  });
+};
+
+
 
 $(blue).click(function(){
-  $(".blue").toggleClass('active');
+  $(".blue").toggleClass("active");
 });
 $(yellow).click(function(){
   $(".yellow").toggleClass('active');
