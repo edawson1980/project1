ff4e919a492e498ee7aadad7a1b26ba768df041b
Erin <erinedawson@gmail.com>
2017-04-06 09:22:18 -0400

    add audio file. add functionality for each color

--- script3.js
+++ script3.js
@@ -14,7 +14,7 @@ var lights = [blue, yellow, red, green];
 var pattern = [];
 var user = [];
 var score = 0;
-var delay = 2000;
+var delay = 800;
 
 lights.toString();
 
@@ -26,37 +26,16 @@ function randomColor(){
   return lights[Math.floor(Math.random()*lights.length)];
 }
 
- // $(".midbutton").click(function(){
- //   console.log("button works");
- //   });
 
 
    $(".midbutton").click(function compMoves() {
     pattern.push(randomColor());
-
-  //   $(blue).addClass("active");
-  //   setTimeout(function(){
-  //  $(blue).removeClass("active");
-  //   }, 250);
-  //   $(yellow).addClass("active");
-  //   setTimeout(function(){
-  //   $(yellow).removeClass("active");
-  //   }, 250);
-  //   $(red).addClass("active");
-  //   setTimeout(function(){
-  //  $(red).removeClass("active");
-  //   }, 250);
-  //   $(green).addClass("active");
-  //   setTimeout(function(){
-  //  $(green).removeClass("active");
-  //   }, 250);
-
       pattern.forEach(function(color, index) {
         setTimeout(function() {
           $(color).addClass('active');
         setTimeout(function() {
           $(color).removeClass('active');
-        }, 1000);
+        }, 500);
       }, delay * index);
     });
 
@@ -65,74 +44,126 @@ function randomColor(){
 
 
 
-  $("blue").click(function userMoves(){
-    setTimeout(function() {
-      $("blue").addClass('active');
-    setTimeout(function() {
-      $("blue").removeClass('active');
-    }, 1000);
+
+  $("blue").click(function userMoves() {
+    user.push($(this).val());
+    if((user.lastIndexOf()) !== (pattern.lastIndexOf())){
+      alert("GAME OVER");
+    } else {
+      pattern.push(randomColor());
+      pattern.forEach(function(color, index) {
+      setTimeout(function() {
+        $(color).addClass('active');
+      setTimeout(function() {
+        $(color).removeClass('active');
+      }, 500);
+    }, delay * index);
   });
-})
-  //     if (userMoves == valueOf[pattern]){
-  //       alert("GAME OVER");
-  //     } else {
-  //       $(".midbutton").click(function compMoves() {
-  //        pattern.push(randomColor());
-  //     });
-  //   }
-  // });
+    }
+  });
+    $(".blue").toggleClass("active");
+    setTimeout(function() {
+      $(".blue").toggleClass('active');
+    }, 500);
 
-    //   console.log("button works");
-    //   });
-
-      setTimeout(function() {
-        $(yellow).addClass('active');
-      setTimeout(function() {
-        $(yellow).removeClass('active');
-      }, 1000);
+    $("yellow").click(function userMoves() {
+      user.push($(this).val());
+      if((user.lastIndexOf()) !== (pattern.lastIndexOf())){
+        alert("GAME OVER");
+      } else {
+        pattern.push(randomColor());
+        pattern.forEach(function(color, index) {
+        setTimeout(function() {
+          $(color).addClass('active');
+        setTimeout(function() {
+          $(color).removeClass('active');
+        }, 500);
+      }, delay * index);
     });
+    $(yellow).click(function(){
+      $(".yellow").toggleClass("active");
+      setTimeout(function() {
+        $(".yellow").toggleClass('active');
+      }, 500);
+    });
+  }
+});
+
+
+$("red").click(function userMoves() {
+  user.push($(this).val());
+  if((user.lastIndexOf()) !== (pattern.lastIndexOf())){
+    alert("GAME OVER");
+  } else {
+    pattern.push(randomColor());
+    pattern.forEach(function(color, index) {
     setTimeout(function() {
-      $(red).addClass('active');
+      $(color).addClass('active');
     setTimeout(function() {
-      $(red).removeClass('active');
-    }, 1000);
-  });
+      $(color).removeClass('active');
+    }, 500);
+  }, delay * index);
+});
+$(red).click(function(){
+  $(".red").toggleClass("active");
   setTimeout(function() {
-    $(green).addClass('active');
+    $(".red").toggleClass('active');
+  }, 500);
+});
+}
+});
+
+$("green").click(function userMoves() {
+  user.push($(this).val());
+  if((user.lastIndexOf()) !== (pattern.lastIndexOf())){
+    alert("GAME OVER");
+  } else {
+    pattern.push(randomColor());
+    pattern.forEach(function(color, index) {
+    setTimeout(function() {
+      $(color).addClass('active');
+    setTimeout(function() {
+      $(color).removeClass('active');
+    }, 500);
+  }, delay * index);
+});
+$(green).click(function(){
+  $(".green").toggleClass("active");
   setTimeout(function() {
-    $(green).removeClass('active');
-  }, 1000);
+    $(".green").toggleClass('active');
+  }, 500);
+});
+}
 });
 
 
 
+  //     setTimeout(function() {
+  //     $(".blue").addClass('active');
+  //     setTimeout(function() {
+  //     $(".blue").removeClass('active');
+  //     }, 1000);
+  //   });
+  //     setTimeout(function() {
+  //       $(yellow).addClass('active');
+  //     setTimeout(function() {
+  //       $(yellow).removeClass('active');
+  //     }, 1000);
+  //   });
+  //   setTimeout(function() {
+  //     $(red).addClass('active');
+  //   setTimeout(function() {
+  //     $(red).removeClass('active');
+  //   }, 1000);
+  // });
+  //   setTimeout(function() {
+  //     $(green).addClass('active');
+  //   setTimeout(function() {
+  //     $(green).removeClass('active');
+  //   }, 1000);
+  // });
 
-// var isClicked = setInterval(active, 750);
-//   clearInterval(isClicked);
-//
-// var userBlue = function(){
-//   $(blue).click(function(){
-//     $(".blue").toggleClass("active");
-//   });
-// };
-//
-// var userYellow = function(){
-//   $(yellow).click(function(){
-//     $(".yellow").toggleClass("active");
-//   });
-// };
-//
-// var userRed = function(){
-//   $(red).click(function(){
-//     $(".red").toggleClass("active");
-//   });
-// };
-//
-// var userGreen = function(){
-//   $(green).click(function(){
-//     $(".green").toggleClass("active");
-//   });
-// };
+
 
 
 
