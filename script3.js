6a2cb32e3a8320c807230115f3896356e8c67d0e
Erin <erinedawson@gmail.com>
2017-07-06 13:48:21 -0400

    update script3 file, game functioning

--- script3.js
+++ script3.js
@@ -1,201 +1,261 @@
-var blue = $(".blue");
-var yellow = $(".yellow");
-var red = $(".red");
-var green = $(".green");
-// var str=$(blue).clone().wrap('<div/>').parent().html();
-// var str=$(yellow).clone().wrap('<div/>').parent().html();
-// var str=$(red).clone().wrap('<div/>').parent().html();
-// var str=$(green).clone().wrap('<div/>').parent().html();
-// var str=$(midbutton).clone().wrap('<div/>').parent().html();
+var btnID = "";
+var seqArray = [];
+var i = 0;
+var running = false;
+var interval = null;
+var playerTurns = 0;
+var winRounds = 20;
 
+var bluesnd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+var yellowsnd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var redsnd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var greensnd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+var wrongAns = new Audio('http://scottemcwilliams.com/imagedep/242503__gabrielaraujo__failure-wrong-action.wav');
 
-var lights = [blue, yellow, red, green];
-var pattern = [];
-var user = [];
-var score = 0;
-var delay = 800;
+$("#power").on('click', function() {
 
+  $("#power").css({"float": "right"});
 
+  if (running) {
+    $("#power").css({"float": "left"});
+    running = false;
+    $("#score").html("off");
+    seqArray = [];
+    i = 0;
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+    return;
+  }
+  running = true;
+  $("#score").html("0");
 
+  $(".midbutton").on('click', function(e) {
 
+    if (!running) {
+      return;
+    }
+    $("#score").html("off");
+    seqArray = [];
+    i = 0;
+    this.i = 0;
+    var btnID = "";
+    var playerTurns = 0;
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
 
-// var audio = $("#mysoundclip")[0];
-// $(".midbutton").click(function() {
-//
-//     console.log("this is the audio button");
-//     audio.play();
-// });
+    nextRnd();
+  });
 
+});
 
-function randomColor() {
-    console.log("colors work");
-    return lights[Math.floor(Math.random() * lights.length)];
+$('.blue').on('mousedown', function() {
+  if (!running) {
+    return;
+  }
+  $(this).addClass('active');
+  bluesnd.play();
+
+}).on('mouseup', function() {
+  if (!running) {
+    return;
+  }
+  $(this).removeClass('active');
+});
+
+$('.yellow').on('mousedown', function() {
+  if (!running) {
+    return;
+  }
+  $(this).addClass('active');
+
+  yellowsnd.play();
+
+}).on('mouseup', function() {
+  if (!running) {
+    return;
+  }
+  $(this).removeClass('active');
+});
+
+$('.red').on('mousedown', function() {
+  if (!running) {
+    return;
+  }
+  $(this).addClass('active');
+
+  redsnd.play();
+
+}).on('mouseup', function() {
+  if (!running) {
+    return;
+  }
+  $(this).removeClass('active');
+});
+
+$('.green').on('mousedown', function() {
+  if (!running) {
+    return;
+  }
+  $(this).addClass('active');
+
+  greensnd.play();
+
+}).on('mouseup', function() {
+  if (!running) {
+    return;
+  }
+  $(this).removeClass('active');
+});
+
+function player(btnPush) {
+
+  console.log("Player");
+
+  if (!running) {
+    return;
+  }
+
+  btnID = ("light" + btnPush);
+  console.log("btnID = " + btnID);
+
+  endCheck(btnPush, playerTurns);
+  playerTurns++;
+
+  if (playerTurns < seqArray.length) {
+    return;
+  }
+
+  nextRnd();
 }
 
+function endCheck(btnPush, i) {
 
+  if (i > winRounds) {
+    alert("winner!");
+    $("#score").html("--");
+    seqArray = [];
+    i = 0;
+    this.i = 0;
+    var btnID = "";
+    var playerTurns = 0;
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
 
-$(".midbutton").click(function compMoves() {
-    pattern.push(randomColor());
-    pattern.forEach(function(color, index) {
-        setTimeout(function() {
-            $(color).addClass('active');
-            setTimeout(function() {
-                $(color).removeClass('active');
-            }, 500);
-        }, delay * index);
-    });
+  } else if ((btnPush !== seqArray[i])) {
+    $("#score").html("!!");
+    wrongAns.play();
+
+    $("#score").html("--");
+    alert('GAME OVER');
+    seqArray = [];
+    this.i = 0;
+    var btnID = "";
+    var playerTurns = 0;
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+
+  }
+
+}
+
+function nextRnd() {
+  $("#score").html(i + 1);
+
+  seqArray.push(getRandomArbitrary(1, 5));
+  playSeq(seqArray);
+  i++;
+}
+
+$(".tile").on('click', function() {
+  if (running == false) {
+    return;
+  }
+
+  btnPush = Number(this.id);
+
+  player(btnPush);
 
 });
 
+function playerTurn() {
+  playerTurns = 0;
+}
 
+function playSeq(seqArray) {
 
+  var j = 0;
 
+  if (interval === null) {
+    interval = setInterval(function() {
+      playIt(seqArray[j]);
+      flashIt(seqArray[j]);
 
-blue.click(function userMoves() {
-    user.push($(this).text());
-    if ((user.length - 1) != (pattern.length - 1)) {
-        alert("IT SURE WOULD");
-    } else {
-        pattern.push(randomColor());
-        pattern.forEach(function(color, index) {
-            setTimeout(function() {
-                $(color).addClass('active');
-                setTimeout(function() {
-                    $(color).removeClass('active');
-                }, 500);
-            }, delay * index);
-        });
-    }
+      j++;
+      if (j >= seqArray.length) {
+        clearInterval(interval);
+        interval = null;
+        playerTurn();
+      }
+    }, 1000);
+  }
+} // End playSeq
 
-    $(".blue").toggleClass('active');
+function flashIt(j) {
+
+  if (j == 1) {
+    $(".blue").addClass('active');
     setTimeout(function() {
-        $(".blue").toggleClass('active');
-    }, 500);
-});
+      $(".blue").removeClass('active');
+    }, 300);
+  };
 
+  if (j == 2) {
+    $(".yellow").addClass('active');
+    setTimeout(function() {
+      $(".yellow").removeClass('active');
+    }, 300);
+  };
 
-yellow.click(function userMoves() {
-    user.push($(this).text());
-    if ((user.length - 1) != (pattern.length - 1)) {
-        alert("BE GREAT");
-    } else {
-        pattern.push(randomColor());
-        pattern.forEach(function(color, index) {
-            setTimeout(function() {
-                $(color).addClass('active');
-                setTimeout(function() {
-                    $(color).removeClass('active');
-                }, 500);
-            }, delay * index);
-        });
-        yellow.click(function() {
-            $(".yellow").toggleClass("active");
-            setTimeout(function() {
-                $(".yellow").toggleClass('active');
-            }, 500);
-        });
-    }
-});
+  if (j == 3) {
+    $(".red").addClass('active');
+    setTimeout(function() {
+      $(".red").removeClass('active');
+    }, 300);
 
+  };
 
-red.click(function userMoves() {
-    user.push($(this).text());
-    if ((user.length - 1) != (pattern.length - 1)) {
-        alert("IF I COULD");
-    } else {
-        pattern.push(randomColor());
-        pattern.forEach(function(color, index) {
-            setTimeout(function() {
-                $(color).addClass('active');
-                setTimeout(function() {
-                    $(color).removeClass('active');
-                }, 500);
-            }, delay * index);
-        });
-        red.click(function() {
-            $(".red").toggleClass("active");
-            setTimeout(function() {
-                $(".red").toggleClass('active');
-            }, 500);
-        });
-    }
-});
+  if (j == 4) {
+    $(".green").addClass('active');
+    setTimeout(function() {
+      $(".green").removeClass('active');
+    }, 300);
+  };
+} // End flashIt
 
-green.click(function userMoves() {
-    user.push($(this).text());
-    if ((user.length - 1) != (pattern.length - 1)) {
-        alert("FIGURE THIS OUT");
-    } else {
-        pattern.push(randomColor());
-        pattern.forEach(function(color, index) {
-            setTimeout(function() {
-                $(color).addClass('active');
-                setTimeout(function() {
-                    $(color).removeClass('active');
-                }, 500);
-            }, delay * index);
-        });
-        green.click(function() {
-            $(".green").toggleClass("active");
-            setTimeout(function() {
-                $(".green").toggleClass('active');
-            }, 500);
-        });
-    }
-});
+function playIt(j) {
+  if (j == 1) {
+    bluesnd.play();
+  };
 
+  if (j == 2) {
+    yellowsnd.play();
+  };
+  if (j == 3) {
+    redsnd.play();
+  };
+  if (j == 4) {
+    greensnd.play();
+  };
+} // End playIt
 
-
-//     setTimeout(function() {
-//     $(".blue").addClass('active');
-//     setTimeout(function() {
-//     $(".blue").removeClass('active');
-//     }, 1000);
-//   });
-//     setTimeout(function() {
-//       $(yellow).addClass('active');
-//     setTimeout(function() {
-//       $(yellow).removeClass('active');
-//     }, 1000);
-//   });
-//   setTimeout(function() {
-//     $(red).addClass('active');
-//   setTimeout(function() {
-//     $(red).removeClass('active');
-//   }, 1000);
-// });
-//   setTimeout(function() {
-//     $(green).addClass('active');
-//   setTimeout(function() {
-//     $(green).removeClass('active');
-//   }, 1000);
-// });
-
-
-
-
-
-// $(blue).click(function(){
-//   $(".blue").toggleClass("active");
-//   setTimeout(function() {
-//     $(".blue").toggleClass('active');
-//   }, 500);
-// });
-// $(yellow).click(function(){
-//   $(".yellow").toggleClass("active");
-//   setTimeout(function() {
-//     $(".yellow").toggleClass('active');
-//   }, 500);
-// });
-// $(red).click(function(){
-//   $(".red").toggleClass("active");
-//   setTimeout(function() {
-//     $(".red").toggleClass('active');
-//   }, 500);
-// });
-// $(green).click(function(){
-//   $(".green").toggleClass("active");
-//   setTimeout(function() {
-//     $(".green").toggleClass('active');
-//   }, 500);
-// });
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+} // End getRandomArbitrary
